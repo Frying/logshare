764509e5d4c0d0c3b00f248b1dbdb1f3d263cc32
Ryno Azhar Wahyudi <ryno.azhar@mindtalk.com>
2016-09-29 13:57:19 +0700

    Prevent 'MySQL server has gone away' message

--- models.nim
+++ models.nim
@@ -1,12 +1,14 @@
 import db_mysql
 
-var db_conn = open("localhost","root","123123","pastans")
-
 proc add*(a:string, b:string, c:string, d:string, e:string) =
+  let db_conn = open("localhost","root","123123","pastans")
   db_conn.exec(sql"start transaction")
   db_conn.exec(sql"insert into log_table(ID, LogSubject, Reporter, ReportDate, LogDetail) values(?,?,?,?,?)", a, b, c, d, e)
   db_conn.exec(sql"commit")
+  close(db_conn)
 
 proc getLogDetail*(id:string) : seq =
+  let db_conn = open("localhost","root","123123","pastans")
   var y = db_conn.getRow(sql"select * from log_table where ID=?", id)
+  close(db_conn)
   return y
